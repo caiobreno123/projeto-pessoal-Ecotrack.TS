e10e65203053fa6b74cbfb43714b791e22cf91a4
caiobreno123 <caiobrenopiresduarte@gmail.com>
2025-12-02 23:03:43 -0300

    Delete src directory

--- src/models/Dispositivo.ts
+++
@@ -1,8 +0,0 @@
-export default class Dispositivo {
-  constructor(
-    public id: number | null,
-    public nome: string,
-    public potencia: number, // watts
-    public horasUso: number // horas por dia
-  ) {}
-}
